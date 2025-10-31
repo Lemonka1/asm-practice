82e8568946b27eacc574b4148ebde6f883267f19
Дарья <dashazubik31@gmail.com>
2025-11-01 00:51:15 +0200

    Update hw5.asm

--- homework/hw5.asm
+++ homework/hw5.asm
@@ -1,1 +1,37 @@
+section .data
+msg db "******************************",10
+    db "*  *                      *  *",10
+    db "*    *                  *    *",10
+    db "*      *              *      *",10
+    db "*        *          *        *",10
+    db "*          *      *          *",10
+    db "*            *  *            *",10
+    db "*             **             *",10
+    db "*           *    *           *",10
+    db "*         *        *         *",10
+    db "*       *            *       *",10
+    db "*     *                *     *",10
+    db "*   *                    *   *",10
+    db "* *                        * *",10
+    db "******************************",10,0
+
+section .text
+global _start
+
+_start:
+    mov rsi, msg
+.next:
+    lodsb              ; зчитати наступний байт у AL
+    cmp al, 0
+    je .done
+    mov rax, 1         ; sys_write
+    mov rdi, 1         ; stdout
+    mov rdx, 1
+    syscall
+    jmp .next
+
+.done:
+    mov rax, 60        ; sys_exit
+    xor rdi, rdi
+    syscall
 
